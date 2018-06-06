ad927e7c784f45ce702f46b31f1bc3dbebcb6c83
Maxim Zimin <zim.ma2010@yandex.ru>
2018-06-06 12:21:46 +0300

    InternShip 4
    Вынесение Database Interact в отдельный Шаблонный класс

---
+++ InternShip1/InternShip1/InternShip1/InternShip1/TypeDAO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternShip1
+{
+    /// <summary>
+    /// Класс для работы с типами в БД
+    /// </summary>
+    class TypeDAO : DatabaseInteract<Type>
+    {
+        /// <summary>
+        /// Конструктор от строки соединения
+        /// </summary>
+        /// <param name="connection">Строка соединиения</param>
+        public TypeDAO(string connection) { ConnectionString = connection; }
+
+        /// <summary>
+        /// Перегрузка сериализации объекта Type
+        /// </summary>
+        /// <param name="reader">Reader</param>
+        /// <returns>Type</returns>
+        public override Type Serialize(SqlDataReader reader)
+        {
+            Type type = new Type() { Id = Convert.ToInt32(reader["Id"]), Name = reader["Name"].ToString() };
+            return type;
+        }
+    }
+}
