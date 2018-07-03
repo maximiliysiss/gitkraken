38e64d7fdf01a9924edb2c8aa2e962a5688385bc
Maxim Zimin <zim.ma2010@yandex.ru>
2018-07-03 19:00:44 +0300

    Change SQL Requests

--- InternShip5/Requests.sql
+++ InternShip5/Requests.sql
@@ -1,28 +1,54 @@
 /*Запрос, который выводит название товара, название типа и название производителя тех товаров цена которых больше 50*/
-select Good.Name as 'Name',Producer.Name as 'Producer',Type.Name as 'Type' from Good,Producer,Type 
-where Type.id = Good.TypeID and Producer.id=Good.ProducerID and Good.Price>50;
+select Good.Name as 'Name', Producer.Name as 'Producer', Type.Name as 'Type' 
+from Good 
+	inner join Producer on Producer.id = Good.ProducerID
+	inner join Type on Type.id = Good.TypeID 
+		where Good.Price>50;
 
 /*Запрос выводит минимальную цену всех товаров*/
-select name, min(price) as 'price' from good group by name;
+select min(price) as 'Min Price of goods' 
+	from Good;
 
 /*Запрос выводит минимальную цену для каждого типа товара*/
-select type.name as 'name', min(good.price) as 'price' from type, good where good.typeID = type.id group by type.name;
+select type.Name, t1.minPrice
+	from (select Good.TypeID, min(price) as minPrice from Good 
+		group by TypeID) as t1
+	inner join type on type.Id = t1.TypeID;
 
 /*Запрос выводит минимальную цену для каждого типа товара сумма цен которых больше 1000*/
-select type.name as 'name', min(good.price) as 'price' from type, good 
-where good.typeID = type.id group by type.name having sum(good.price)>1000;
+select type.Name, t1.minPrice
+	from (select Good.TypeID, min(price) as minPrice from Good 
+		group by TypeID 
+		having sum(price)>1000) as t1
+	inner join type on type.Id = t1.TypeID;
 
 /*Запрос выводит количество складов для каждого менеджера*/
-select manager.name as 'name', count(stock.id) as 'count' from manager, stock where stock.managerID=manager.id group by manager.name;
+select Manager.Name, t1.CountSt 
+	from (select Stock.ManagerID, count(Stock.Id) as CountSt 
+		from Stock group by ManagerID) as t1
+	inner join Manager on Manager.Id = t1.ManagerId;
 
 /*Запрос выводит менеджеров, чьи склады  обслуживюат более одного магазина*/
-select manager.name as 'name' from stock join manager on manager.id = stock.managerId join stocksforshop on 
-stocksforshop.stockID = stock.id  group by manager.name having count(stocksforshop.shopID)>1;
+select Manager.Name from
+	(select Manager.Id from Stock 
+	inner join Manager on Manager.Id = Stock.ManagerID
+	inner join StocksForShop on StocksForShop.StockID = Stock.Id 
+		group by Manager.Id having count(StocksForShop.ShopID)>1) as t1
+	inner join Manager on Manager.Id = t1.Id;
 
 /*Вывести топ 3 менеджеров на чьих складах большее количество товаров*/
-select top(3) manager.name as 'name' from stock join goodsinstock on  goodsinstock.stockId = stock.id join 
-manager on manager.id = stock.managerId group by manager.name order by sum(goodsinstock.count);
+select top(3) Manager.Name from 
+	(select sum(GoodsInStock.Count) as CountGoods, Manager.Id from Stock 
+	inner join Manager on Manager.Id = Stock.ManagerID
+	inner join GoodsInStock on GoodsInStock.StockID = Stock.Id 
+		group by Manager.Id) as t1
+	inner join Manager on Manager.Id = t1.Id 
+		order by t1.CountGoods;
 
 /*Увеличить на 1 количество всех товаров на складах с именем товара «Фисташки» и которые обслуживают магазины с именем «Мир пустоты»*/
-update goodsinstock set count=count+1 where (select id from good where name=N'Фисташки')=goodID and 
-	(select id from shop where name=N'Мир пустоты') in (select shopID from stocksforshop where stockID = goodsinstock.stockID);
+update goodsinstock set count=count+1 
+	from goodsinstock 
+		inner join Good on Good.Id = GoodsInStock.GoodID
+		inner join StocksForShop on StocksForShop.StockID = GoodsInStock.StockID
+		inner join Shop on StocksForShop.ShopID = Shop.Id
+			where Good.Name = N'Фисташки' and Shop.Name = N'Мир пустоты';
